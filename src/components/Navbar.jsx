23ec47e4b17e01c0c89827d19cb30ec5b0dfaf74
Pawel Bogusz1 <pbog2000@gmail.com>
2024-02-01 20:56:24 +0100

    COM6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 				</li>
 				<li>
 				<Link to='/login'>
-              <img src="./public/minion.png" class="user-icon" width="34" height="34">
+              <img src="./public/minion.png" class="user-icon" width="40" height="44">
                           </img></Link>
             
 				</li>
